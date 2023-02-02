4a70d03ba15fd750e3903ad1a99c746b7bc6a19f
Ethan Jacobs <jacobethan1992@gmail.com>
2023-02-02 15:33:07 -0500

    finished exercise

---
+++ Methods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operators
+{
+    internal class Methods
+    {
+        public static double AreaOfCircle(double radius)
+        {
+           double area = (Math.PI * radius * radius);
+            return area;
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//----------------------------Exercise-1--------------------
+
+using operators;
+using System.Diagnostics.CodeAnalysis;
+
+int a = 17;
+int b = 4;
+int sum = a + b;
+int diff = a- b;
+double quotient = a / b;
+int product = a * b;
+double remainder = a % b;
+Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+//--------------------------Exercise-2--------------------------
+
+Console.WriteLine("what is the radius of my circle?");
+var userResponce= Console.ReadLine();
+var radius= double.Parse(userResponce);
+var answer= (Methods.AreaOfCircle(radius));
+Console.WriteLine($"the are of a circle with the radius of {radius} is {answer}");
